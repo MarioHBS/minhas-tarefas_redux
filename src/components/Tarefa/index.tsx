5a3192179a5b39568be84b05a4d0b718eac25b48
Mário Henrique <marioh90@gmail.com>
2024-04-20 19:58:14 -0300

    > aula 31.6
    * construindo Tarefas passando valores em lista

--- src/components/Tarefa/index.tsx
+++ src/components/Tarefa/index.tsx
@@ -1,16 +1,37 @@
+import { useState } from 'react'
 import * as S from './styles'
 
-const TaskComponent = () => (
-  <S.CardTask>
-    <S.TitleTask>Nome da Terefa</S.TitleTask>
-    <S.TagTask>importante</S.TagTask>
-    <S.TagTask>pendente</S.TagTask>
-    <S.DescriptionTask />
-    <S.ActionBarTask>
-      <S.ButtonTask>Editar</S.ButtonTask>
-      <S.ButtonTask>Remover</S.ButtonTask>
-    </S.ActionBarTask>
-  </S.CardTask>
-)
+export type TaskType = {
+  title: string
+  description: string
+  priority: string
+  status: string
+}
+
+const TaskComponent = ({ description, priority, status, title }: TaskType) => {
+  const [isEditing, setEdit] = useState(false)
+
+  return (
+    <S.CardTask>
+      <S.TitleTask>{title}</S.TitleTask>
+      <S.TagTask>{status}</S.TagTask>
+      <S.TagTask>{priority}</S.TagTask>
+      <S.DescriptionTask value={description} />
+      <S.ActionBarTask>
+        {isEditing ? (
+          <>
+            <S.ButtonTask onClick={() => setEdit(false)}>Salvar</S.ButtonTask>
+            <S.ButtonTask>Cancelar</S.ButtonTask>
+          </>
+        ) : (
+          <>
+            <S.ButtonTask onClick={() => setEdit(true)}>Editar</S.ButtonTask>
+            <S.ButtonTask>Remover</S.ButtonTask>
+          </>
+        )}
+      </S.ActionBarTask>
+    </S.CardTask>
+  )
+}
 
 export default TaskComponent
--- src/containers/ListaTarefas/index.tsx
+++ src/containers/ListaTarefas/index.tsx
@@ -1,28 +1,53 @@
-import TaskComponent from '../../components/Tarefa'
+import TaskComponent, { TaskType } from '../../components/Tarefa'
 import { Container } from './styles'
+
+const tasks: TaskType[] = [
+  {
+    title: 'Estudar TypesScript',
+    description: 'Ver a aula 3 da EBAC',
+    priority: 'importante',
+    status: 'pendente'
+  },
+  {
+    title: 'Pagar conta de Internet',
+    description: 'Baixar fatura no Email',
+    priority: 'urgente',
+    status: 'concluída'
+  },
+  {
+    title: 'Ir para a academia',
+    description: 'Fazer treino B',
+    priority: 'importante',
+    status: 'pendente'
+  },
+  {
+    title: 'Estudar Redux',
+    description: 'Ver a aula 5 da EBAC',
+    priority: 'importante',
+    status: 'editando'
+  },
+  {
+    title: 'Ir para a academia',
+    description: 'Fazer treino A',
+    priority: 'normal',
+    status: 'pendente'
+  }
+]
 
 const ListaDeTarefas = () => (
   <Container>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
     <ul>
-      <li>
-        <TaskComponent />
-      </li>
-      <li>
-        <TaskComponent />
-      </li>
-      <li>
-        <TaskComponent />
-      </li>
-      <li>
-        <TaskComponent />
-      </li>
-      <li>
-        <TaskComponent />
-      </li>
-      <li>
-        <TaskComponent />
-      </li>
+      {tasks.map((task) => (
+        <li key={task.title}>
+          <TaskComponent
+            title={task.title}
+            description={task.description}
+            status={task.status}
+            priority={task.priority}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
